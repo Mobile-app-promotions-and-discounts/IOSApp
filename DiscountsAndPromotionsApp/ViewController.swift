419c1356d5d5e1bc3b9a5049aaa9d4265097374d
Pavel Afanasev <afanasyevpv9@gmail.com>
2023-11-13 22:08:20 +0300

    Initial Commit

---
+++ DiscountsAndPromotionsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiscountsAndPromotionsApp
+//
+//  Created by Павел Афанасьев on 13.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
