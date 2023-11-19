a2707f3aa8c54a7bee184c0f8b90470c83bcf31f
Aleksei Iakushev <av.yakush@gmail.com>
2023-11-20 00:07:36 +0300

    Добавлен view controller экрана сканера

---
+++ DiscountsAndPromotionsApp/UI/ScanViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScanViewController.swift
+//  DiscountsAndPromotionsApp
+//
+//  Created by Aleksey Yakushev on 20.11.2023.
+//
+
+import UIKit
+
+class ScanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
