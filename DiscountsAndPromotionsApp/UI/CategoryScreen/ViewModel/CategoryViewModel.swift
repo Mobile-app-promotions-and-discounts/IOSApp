4009a22c10d84650a3781e7b391eb9a19560572d
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-11-26 21:13:25 +0300

    Правки по итогам ревью: - Package.resolved и Podfile.lock теперь в гите храним - поправлено разграничение ответственности между viewcontroller и viewmodel на главном экране и экране продукты по категории

--- DiscountsAndPromotionsApp/UI/CategoryScreen/ViewModel/CategoryViewModel.swift
+++ DiscountsAndPromotionsApp/UI/CategoryScreen/ViewModel/CategoryViewModel.swift
@@ -1,25 +1,40 @@
 import Foundation
+import Combine
 
 final class CategoryViewModel: CategoryViewModelProtocol {
+    private (set) var productsUpdate = PassthroughSubject<[Product], Never>()
 
-    private (set) var productsList = [Product]()
-
-    func viewDidLoad() {
-        self.productsList = MockDataService.shared.getProductsList()
-    }
-
-    func getNumberOfItemsInSection(section: Int) -> Int {
-        switch section {
-        case 0:
-            return productsList.count
-        default:
-            return 0
+    @Published private var products = [Product]() {
+        didSet {
+            productsUpdate.send(products)
         }
     }
 
+    private var dataService: DataServiceProtocol
+    private var cancellables = Set<AnyCancellable>()
+
+    init(dataService: DataServiceProtocol = MockDataService()) {
+        self.dataService = dataService
+    }
+
+    func viewDidLoad() {
+        fetchData()
+    }
+
+    func numberOfItems() -> Int {
+        return products.count
+    }
+
     func getProduct(for index: Int) -> CategoryCellUIModel {
-        let product = productsList[index]
+        let product = products[index]
         return convertModels(for: product)
+    }
+
+    private func fetchData() {
+        dataService.getProductsList()
+            .receive(on: RunLoop.main)
+            .assign(to: \.products, on: self)
+            .store(in: &cancellables)
     }
 
     private func convertModels(for product: Product) -> CategoryCellUIModel {
--- DiscountsAndPromotionsApp/UI/MainScreen/ViewModel/MainViewModel.swift
+++ DiscountsAndPromotionsApp/UI/MainScreen/ViewModel/MainViewModel.swift
@@ -1,34 +1,59 @@
 import Foundation
+import Combine
 
-final class MainViewModel: MainViewModelProtocol {
-
-    private (set) var categoriesList = [Category]()
-    private (set) var promotionList = [Product]()
-    private (set) var storeList = [Store]()
-
-    func viewDidLoad() {
-        self.categoriesList = MockDataService.shared.getCategoriesList()
-        self.promotionList = MockDataService.shared.getProductsList()
-        self.storeList = MockDataService.shared.getStoresList()
+final class MainViewModel: MainViewModelProtocol, ObservableObject {
+    var numberOfSections: Int {
+        return MainSection.allCases.count
     }
 
-    func getNumberOfItemsInSection(section: Int) -> Int {
+    private (set) var categoriesUpdate = PassthroughSubject<[Category], Never>()
+    private (set) var productsUpdate = PassthroughSubject<[Product], Never>()
+    private (set) var storesUpdate = PassthroughSubject<[Store], Never>()
+
+    @Published private var categories = [Category]() {
+        didSet {
+            categoriesUpdate.send(categories)
+        }
+    }
+
+    @Published private var products = [Product]() {
+        didSet {
+            productsUpdate.send(products)
+        }
+    }
+
+    @Published private var stores = [Store]() {
+        didSet {
+            storesUpdate.send(stores)
+        }
+    }
+
+    private var dataService: DataServiceProtocol
+    private var cancellables = Set<AnyCancellable>()
+
+    init(dataService: DataServiceProtocol = MockDataService()) {
+        self.dataService = dataService
+    }
+
+    func viewDidLoad() {
+        fetchData()
+    }
+
+    func numberOfItems(inSection section: MainSection) -> Int {
         switch section {
-        case 0:
-            return categoriesList.count
-        case 1:
-            return promotionList.count
-        case 2:
-            return storeList.count
-        default:
-            return 0
+        case .categories:
+            return categories.count
+        case .promotions:
+            return products.count
+        case .stores:
+            return stores.count
         }
     }
 
     func getTitleFor(indexPath: IndexPath) -> String {
         switch indexPath.section {
         case 0:
-            return categoriesList[indexPath.row].name
+            return categories[indexPath.row].name
         case 1:
             return NSLocalizedString("Promotions", tableName: "MainFlow", comment: "")
         case 2:
@@ -39,13 +64,30 @@ final class MainViewModel: MainViewModelProtocol {
     }
 
     func getPromotion(for index: Int) -> PromotionUIModel {
-        let product = promotionList[index]
+        let product = products[index]
         return convertPromotionModel(for: product)
     }
 
     func getStore(for index: Int) -> StoreUIModel {
-        let store = storeList[index]
+        let store = stores[index]
         return convertStoreModel(for: store)
+    }
+
+    private func fetchData() {
+        dataService.getCategoriesList()
+            .receive(on: RunLoop.main)
+            .assign(to: \.categories, on: self)
+            .store(in: &cancellables)
+
+        dataService.getProductsList()
+            .receive(on: RunLoop.main)
+            .assign(to: \.products, on: self)
+            .store(in: &cancellables)
+
+        dataService.getStoresList()
+            .receive(on: RunLoop.main)
+            .assign(to: \.stores, on: self)
+            .store(in: &cancellables)
     }
 
     private func convertPromotionModel(for product: Product) -> PromotionUIModel {
