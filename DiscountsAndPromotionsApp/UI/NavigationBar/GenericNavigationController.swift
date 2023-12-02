70e5a2aaee287daa4014ff457bdf15d61f314b96
Aleksei Iakushev <av.yakush@gmail.com>
2023-12-03 02:41:19 +0300

    Настройка Search Bar согласно дизайну

--- DiscountsAndPromotionsApp/UI/NavigationBar/GenericNavigationController.swift
+++ DiscountsAndPromotionsApp/UI/NavigationBar/GenericNavigationController.swift
@@ -2,11 +2,16 @@ import UIKit
 
 final class GenericNavigationController: UINavigationController {
     private var roundedBackground = UIView()
+    private var clippedView = UIView()
     private var navBarFrame = CGRect()
     private let cornerRadius = CornerRadius.regular.cgFloat()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavBar()
+    }
+
+    private func setupNavBar() {
         let appearence = UINavigationBarAppearance()
         appearence.configureWithTransparentBackground()
         appearence.backgroundColor = .cherryMainAccent
@@ -15,19 +20,28 @@ final class GenericNavigationController: UINavigationController {
         navigationBar.scrollEdgeAppearance = appearence
         navigationBar.compactAppearance = appearence
 
+        navigationBar.barStyle = .black
         navigationBar.tintColor = .cherryWhite
 
-        navigationBar.addSubview(roundedBackground)
-        roundedBackground.backgroundColor = UIColor.cherryMainAccent
+        roundedBackground.backgroundColor = .cherryMainAccent
         roundedBackground.layer.cornerRadius = cornerRadius
         roundedBackground.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+        clippedView.backgroundColor = .clear
+        clippedView.clipsToBounds = true
+        navigationBar.addSubview(clippedView)
+        clippedView.addSubview(roundedBackground)
     }
+
     override func viewDidLayoutSubviews() {
-        navBarFrame = CGRect(origin: CGPoint(x: navigationBar.bounds.minX,
-                                             y: navigationBar.bounds.maxY - cornerRadius),
+        navBarFrame = CGRect(origin: CGPoint(x: 0,
+                                             y: -cornerRadius),
                                  size: CGSize(width: navigationBar.bounds.width,
                                               height: cornerRadius*2))
         roundedBackground.frame = navBarFrame
-        navigationBar.sendSubviewToBack(roundedBackground)
+        let clippedFrame = CGRect(origin: CGPoint(x: navigationBar.bounds.minX,
+                                               y: navigationBar.bounds.maxY),
+                                   size: CGSize(width: navigationBar.bounds.width,
+                                                height: cornerRadius))
+        clippedView.frame = clippedFrame
     }
 }
--- DiscountsAndPromotionsApp/UI/NavigationBar/SearchEnabledViewController.swift
+++ DiscountsAndPromotionsApp/UI/NavigationBar/SearchEnabledViewController.swift
@@ -10,8 +10,26 @@ class SearchEnabledViewController: UIViewController {
 
     private func setupSearchBar() {
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
-        searchBar.placeholder = NSLocalizedString("searchPlaceholder", tableName: "MainFlow", comment: "")
+
         searchBar.searchTextField.backgroundColor = .cherryWhite
+        let placeholderAttributes = [NSAttributedString.Key.font: CherryFonts.inputSmall,
+                          NSAttributedString.Key.foregroundColor: UIColor.cherryGrayBlue]
+        let textAttributes = [NSAttributedString.Key.font: CherryFonts.inputSmall,
+                          NSAttributedString.Key.foregroundColor: UIColor.cherryBlack]
+
+        searchBar.searchTextField.defaultTextAttributes = textAttributes as [NSAttributedString.Key: Any]
+        searchBar.searchTextField.typingAttributes = textAttributes as [NSAttributedString.Key: Any]
+        searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: NSLocalizedString("searchPlaceholder",
+                                                                                                       tableName: "MainFlow",
+                                                                                                       comment: ""),
+                                                                             attributes: placeholderAttributes as [NSAttributedString.Key: Any])
+        searchBar.setImage(.searchIcon, for: .search, state: .normal)
+        searchBar.setImage(.icClose, for: .clear, state: .normal)
+        searchBar.barStyle = .black
+        searchBar.backgroundColor = .clear
+        searchBar.tintColor = .cherryMainAccent
+        searchBar.layer.cornerRadius = CornerRadius.regular.cgFloat()
+        searchBar.layer.masksToBounds = true
         navigationItem.titleView = searchBar
         searchBar.delegate = self
     }
