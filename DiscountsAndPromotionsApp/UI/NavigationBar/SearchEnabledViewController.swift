820bf1237c15d7e18481b9f29bdfd2248bbc9fc0
Aleksei Iakushev <av.yakush@gmail.com>
2023-12-03 04:40:03 +0300

    Исправлен цвет текста

--- DiscountsAndPromotionsApp/UI/NavigationBar/SearchEnabledViewController.swift
+++ DiscountsAndPromotionsApp/UI/NavigationBar/SearchEnabledViewController.swift
@@ -30,8 +30,7 @@ class SearchEnabledViewController: UIViewController {
         searchBar.searchTextField.backgroundColor = .cherryWhite
         let placeholderAttributes = [NSAttributedString.Key.font: CherryFonts.inputSmall,
                                      NSAttributedString.Key.foregroundColor: UIColor.cherryGrayBlue]
-        let textAttributes = [NSAttributedString.Key.font: CherryFonts.inputSmall,
-                              NSAttributedString.Key.foregroundColor: UIColor.cherryBlack]
+        let textAttributes = [NSAttributedString.Key.font: CherryFonts.inputSmall]
 
         searchBar.searchTextField.defaultTextAttributes = textAttributes as [NSAttributedString.Key: Any]
         searchBar.searchTextField.textColor = .cherryBlack
@@ -42,7 +41,6 @@ class SearchEnabledViewController: UIViewController {
                                                                              attributes: placeholderAttributes as [NSAttributedString.Key: Any])
         searchBar.setImage(.searchIcon, for: .search, state: .normal)
         searchBar.setImage(.icClose, for: .clear, state: .normal)
-        searchBar.barStyle = .black
         searchBar.backgroundColor = .clear
         searchBar.tintColor = .cherryMainAccent
         searchBar.layer.cornerRadius = CornerRadius.regular.cgFloat()
