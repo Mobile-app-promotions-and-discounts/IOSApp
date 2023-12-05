274957e7773bd173b4ecea86bcd7e75af609875b
Denis Chakyr <ChakyrIT@gmail.com>
2023-12-05 19:29:19 +0200

    Merging dev into ProductCard

--- DiscountsAndPromotionsApp/Extensions/UIImageExtensions.swift
+++ DiscountsAndPromotionsApp/Extensions/UIImageExtensions.swift
@@ -1,19 +1,19 @@
-import UIKit
-
-extension UIImage {
-    func withVerticallyFlippedOrientation() -> UIImage {
-        var flippedImage = UIImage()
-        if let newCGImage = self.cgImage {
-            flippedImage = UIImage(cgImage: newCGImage, scale: self.scale, orientation: .downMirrored)
-        }
-        return flippedImage
-    }
-
-    func resizedImage(Size newSize: CGSize) -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
-        self.draw(in: CGRect(origin: .zero, size: newSize))
-        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return resizedImage
-    }
-}
+// import UIKit
+//
+// extension UIImage {
+//    func withVerticallyFlippedOrientation() -> UIImage {
+//        var flippedImage = UIImage()
+//        if let newCGImage = self.cgImage {
+//            flippedImage = UIImage(cgImage: newCGImage, scale: self.scale, orientation: .downMirrored)
+//        }
+//        return flippedImage
+//    }
+//
+//    func resizedImage(Size newSize: CGSize) -> UIImage? {
+//        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+//        self.draw(in: CGRect(origin: .zero, size: newSize))
+//        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        return resizedImage
+//    }
+// }
