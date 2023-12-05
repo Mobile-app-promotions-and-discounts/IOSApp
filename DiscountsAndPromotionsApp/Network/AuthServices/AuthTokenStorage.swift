63a66516f63f9d7c2967e4d69bf93ccbda34128a
Aleksei Iakushev <av.yakush@gmail.com>
2023-12-05 19:52:22 +0300

    Добавлен SwiftKeychainWrapper и сервис хранения токенов

---
+++ DiscountsAndPromotionsApp/Network/AuthServices/AuthTokenStorage.swift
@@ -0,0 +1,29 @@
+import Foundation
+import SwiftKeychainWrapper
+
+final class AuthTokenStorage {
+    private let wrapper = KeychainWrapper.standard
+    
+    private enum Keys: String {
+        case refreshToken
+        case accessToken
+    }
+    
+    var accessToken: String? {
+        get {
+            wrapper.string(forKey: Keys.accessToken.rawValue)
+        }
+        set {
+            guard let newToken = newValue else { return }
+            let isSuccess = wrapper.set(newToken, forKey: Keys.accessToken.rawValue)
+            
+            if !isSuccess {
+                ErrorHandler.handle(error: .customError("Access token saving error"))
+            }
+        }
+    }
+    
+    func clearTokenStorage() {
+        wrapper.remove(forKey: KeychainWrapper.Key(rawValue: Keys.accessToken.rawValue))
+    }
+}
